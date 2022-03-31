a60f84f7f03fcec74e671c79cf89d42b83ef2f32
unknown <cantilloderek@gmail.com>
2022-03-31 11:57:30 -0500

    animate.css

--- src/Components/hero/HeroList.jsx
+++ src/Components/hero/HeroList.jsx
@@ -7,7 +7,7 @@ export const HeroList = ({publisher}) => {
     const heroes = useMemo(() =>  getHeroesByPublisher(publisher), [publisher])
   return (
 
-    <div className='row rows-cols-1 row-cols-md-3 g-3 p-5'>
+    <div className='row rows-cols-1 row-cols-md-3 g-3 p-5 animate__animated animate__fadeIn'>
         
         
 
--- src/Components/hero/HeroeScreen.jsx
+++ src/Components/hero/HeroeScreen.jsx
@@ -21,11 +21,11 @@ export const HeroeScreen = () => {
   }
   return (
     <div className='row mt-5'>
-      <div className='col-4'>
+      <div className='col-4 animate__animated animate__bounceInLeft'>
         <img src={imagePath} alt={hero.superhero} className='img-thumbnail'/>
   
       </div>
-      <div className='col-8'>
+      <div className='col-8 animate__animated animate__fadeIn'>
         <h3>{hero.superhero}</h3>
         <ul className='list-group list-group-flush'>
           <li className='list-group-item'><b>Alter ego: </b>{hero.alter_ego}</li>
